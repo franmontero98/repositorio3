7a467caf59660d5fb5ea24d53170a7b101a696bd
franmontero98 <36838406+franmontero98@users.noreply.github.com>
2018-03-20 09:48:17 +0100

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Random;
+
+public class Array {
+	
+	int tabla[] = new int [11];
+	Random rnd = new Random();
+	int i, x = 0;
+	
+	public void rellenararray() {
+
+		for (i = 1; i <= 10; i++) {
+			x = (int) (rnd.nextDouble()*20);
+			tabla[i] = x + 1; 
+			}
+			
+	}
+	
+	public void operararray() {
+		System.out.println(tabla[i] + tabla[i]);
+	}
+	
+	public static void main(String[] args) {
+		Array array1;
+		array1 = new Array();
+		array1.rellenararray();
+		array1.operararray();
+		}
+	
+}
+
